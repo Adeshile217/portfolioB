6475b8ef73078ac21c7eaf23a062772eedce4d22
Adeshile Ibraheem <sadeshile@gmail.com>
2025-05-20 21:34:29 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,72 @@
+function setCircularProgress(containerId, percent) {
+    if (!containerId) {
+        return;
+    }
+
+    const container = document.querySelector(`#${containerId}`).parentElement;
+    const circle = container.querySelector('.circle-progress');
+    const percentage = container.querySelector('.percentage-text');
+    const radius = circle.r.baseVal.value;
+    const circumference = 2 * Math.PI * radius;
+
+
+
+    percent = Math.min(Math.max(percent, 0), 100);
+    circle.style.strokeDasharray = circumference;
+    const offset = circumference - (percent / 100) * circumference;
+    circle.style.strokeDashoffset = offset;
+
+    percentage.innerHTML = percent + '%';
+}
+
+document.querySelectorAll('.circle-progress').forEach(circle => {
+    const radius = circle.r.baseVal.value;
+    circle.style.strokeDasharray = 2 * Math.PI * radius;
+    circle.style.strokeDashoffset = 2 * Math.PI * radius;
+});
+
+setTimeout(() => {
+    setCircularProgress('frenchPercentage', 100);
+    setCircularProgress('englishPercentage', 90);
+    setCircularProgress('arabicPercentage', 30);
+}, 100);
+
+
+
+function setHorizontalProgress(barId, percent) {
+    const container = document.getElementById(barId);
+
+    if (!container) {
+        return
+    };
+    const fill = container.querySelector('.progress-bar-fill');
+    const percentageText = container.querySelector('.HorizontalProgressPercentage');
+
+    percent = Math.min(Math.max(percent, 0), 100);
+    fill.style.width = percent + '%';
+    percentageText.innerHTML = percent + '%';
+  }
+
+  setHorizontalProgress('htmlProgressbar', 85);
+  setHorizontalProgress('cssProgressbar', 85);
+  setHorizontalProgress('jsProgressbar', 70);
+  setHorizontalProgress('coreldrawProgressbar', 80);
+  setHorizontalProgress('wordpressProgressbar', 85);
+  setHorizontalProgress('primaveraProgressbar', 65);
+
+
+
+const downloadButton = document.getElementById('downloadButton').addEventListener('click', (e)=>{
+    e.preventDefault()
+
+    alert('There is no file to download')
+})
+
+
+
+const form = document.getElementById('form');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Your message has been sent')
+    form.reset();
+  });
